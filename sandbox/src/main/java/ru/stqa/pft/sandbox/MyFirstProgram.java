0bcc51bb25a60438e00646c55bef56e6061ce8b7
cvetatest <cveta.panina@gmail.com>
2021-10-03 12:06:34 +0300

    Создание классов Square и Rectangle по лекции 1

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,21 +5,17 @@ public class MyFirstProgram {
       hello("world");
       hello("user");
       hello("Sveta");
-      double len=5;
-      System.out.println("Площадь квадрата со стороной "+len+"="+area(len));
 
-      double a=4;
-      double b=6;
-        System.out.println("Площадь прямоугольника со сторонами " +a+" и "+b+" = "+area(a,b));
+      Square s=new Square(5);
+      System.out.println("Площадь квадрата со стороной "+s.l+"="+s.area());
+
+      Rectangle r=new Rectangle(4,6);
+      System.out.println("Площадь прямоугольника со сторонами " +r.a+" и "+r.b+" = "+r.area());
 
     }
     public static void hello(String somebody){
         System.out.println(" Hello," +somebody+"!");
     }
-    public static double area(double len){
-        return len * len;
-    }
-    public static double area(double a,double b){
-        return a*b;
-    }
+
+
 }
