938d79074cc93913ffe661daf166092e0ddcde27
cvetatest <cveta.panina@gmail.com>
2021-10-04 00:05:49 +0300

    Задание №2: Потренироваться использовать функции, классы, объекты и методы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,21 +1,32 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args){
-      hello("world");
-      hello("user");
-      hello("Sveta");
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Sveta");
 
-      Square s=new Square(5);
-      System.out.println("Площадь квадрата со стороной "+s.l+"="+s.area());
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + "=" + s.area());
 
-      Rectangle r=new Rectangle(4,6);
-      System.out.println("Площадь прямоугольника со сторонами " +r.a+" и "+r.b+" = "+r.area());
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-    }
-    public static void hello(String somebody){
-        System.out.println(" Hello," +somebody+"!");
-    }
+    Point p1 = new Point(1, 2);
+    Point p2 = new Point(4, 3);
+    double p;
+    p=Point.distance(p1,p2);
+    System.out.println("Расстояние между точками ( " + p1.x + "," + p1.y + ")" + " и " + "( " + p2.x + "," + p2.y + ")" + "=" + p);
+  }
+
+  public static void hello(String somebody) {
+    System.out.println(" Hello," + somebody + "!");
+  }
+
+/* public static double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow((p2.x - p1.x),2) + Math.pow((p2.y - p1.y),2));
+
+  }*/
 
 
 }
