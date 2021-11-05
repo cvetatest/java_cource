79674835f1932cceb75ff9b2e8515a7a95ba2d76
cvetatest <cveta.panina@gmail.com>
2021-11-05 20:26:29 +0300

    Сравнение списков для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,22 +1,62 @@
 package ru.stqa.pft.addressbook.model;
 
+import org.testng.annotations.BeforeMethod;
+
+import java.util.Objects;
+
 public class GroupData {
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
-  public GroupData(String name, String header, String footer) {
+  public GroupData(int id,String name, String header, String footer) {
+    this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public String getName() {
-    return name;
+
+  public GroupData(String name, String header, String footer) {
+    this.id = 0;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
   }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() { return name; }
 
   public String getHeader() {
     return header;
+  }
+
+
+  public int getId() { return id; }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return id == groupData.id && Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name);
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            '}';
   }
 
   public String getFooter() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,14 +1,33 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.HashSet;
+import java.util.List;
 
 public class GroupCreationTests extends TestBase{
 
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    List<GroupData> before =app.getGroupHelper().getGroupList();
+    GroupData group=new GroupData("test1", null, null);
+    app.getGroupHelper().createGroup(group);
+    List<GroupData> after =app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(),before.size()+1);
+
+
+int max=0;
+for(GroupData g:after){
+  if(g.getId()>max){
+    max=g.getId();
+  }
+}
+group.setId(max);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
     app.logout();
   }
 
