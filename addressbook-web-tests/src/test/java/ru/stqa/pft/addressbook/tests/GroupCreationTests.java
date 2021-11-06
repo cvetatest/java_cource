8eb49851f4a591bae3ea088e8f8d91c6007fcaa3
cvetatest <cveta.panina@gmail.com>
2021-11-06 11:20:18 +0300

    Замена на лямбда выражение при вычислении max id

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,30 +4,24 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
-public class GroupCreationTests extends TestBase{
+public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    List<GroupData> before =app.getGroupHelper().getGroupList();
-    GroupData group=new GroupData("test1", null, null);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = new GroupData("test2", null, null);
     app.getGroupHelper().createGroup(group);
-    List<GroupData> after =app.getGroupHelper().getGroupList();
-    Assert.assertEquals(after.size(),before.size()+1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() + 1);
 
-
-int max=0;
-for(GroupData g:after){
-  if(g.getId()>max){
-    max=g.getId();
-  }
-}
-group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     app.logout();
   }
 
